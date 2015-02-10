ee2af1902c3d13ee020aa65bd620c16d2a629a7f
Ettore Delprino <edelprino@gmail.com>
2015-02-10 12:05:43 +0100

    Added first timeout tests

--- test/pom.js
+++ test/pom.js
@@ -4,13 +4,19 @@ var pom = require('../lib/pom');
 describe('pom', function(){
     it('should execute start commands when pom run', function(){
         commands = {
-            start: ['start-command'],
-            end: ['end-command']
+            start: function() {assert.ok(true)},
+            end: function() {assert.fail()}
         };
-
-        pom = new Pom(commander);
         pom.run(32, commands);
-
-        assert.ok(true);
     })
+
+    it('should execute end commands when pom ended', function(done){
+        this.timeout(600);
+        commands = {
+            start: function() {assert.ok(true)},
+            end: function() {assert.ok(false);done()}
+        };
+        pom.run(0.01, commands);
+    })
+
 })
